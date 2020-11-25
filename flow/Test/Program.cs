1022f2fc07281a05d317e5760e20be6e3ce59c07
Feng Zhou <v-fengzhou@microsoft.com>
2020-11-25 16:41:18 +0800

    test

--- flow/Test/Program.cs
+++ flow/Test/Program.cs
@@ -11,10 +11,12 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Command.Run(@"C:\Users\v-fengzhou\source\repos\paloma", @"CreatePackage.cmd Debug", out int exitCode, out string standOutput);
+            Command.Run(@"C:\Users\v-fengzhou\source\repos\wae", @"CreatePackage.cmd Debug", out int exitCode, out string standOutput);
 
-            Console.WriteLine($"exitCode: {exitCode}");
-            Console.WriteLine($"standOutput: {standOutput}");
+            Console.WriteLine($"exitCode:{Environment.NewLine}{exitCode}");
+            Console.WriteLine($"standOutput:{Environment.NewLine}{standOutput}");
+
+            //Command.RunOneWDK("","",out int x,out string y);
 
             Console.ReadKey();
         }
--- flow/Utilities/Command.cs
+++ flow/Utilities/Command.cs
@@ -10,9 +10,19 @@ namespace Utilities
 {
     public class Command
     {
-        private const string CMDPATH = @"C:\Windows\System32\cmd.exe";
+        private const string CMDPATH = @"cmd.exe";
+        //private const string CMDTITLE = @"title Command Prompt";
+        private const string EWDKPATH = @"C:\17134.1.3";
+        private const string EWDKCMD = @"LaunchBuildEnv.cmd";
+        private const string CMD = "cmd";
+        private const string MAINWINDOWSTIELE = @"Administrator:  ""Vs2017 & WDK Build Env WDKContentRoot: C:\17134.1.3\Program Files\Windows Kits\10\""";
+        private static readonly string REPOSFOLDER = $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\source\repos\";
+        private const string OPENREPOSSLN = @"""C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"" Provisioning.sln";
+        private const string REPOSSLN = @"Provisioning.sln";
+        private const string CREATEPACAKGE = @"CreatePackage.cmd Debug";
+        private const string CREATEPACAKGECMD = @"CreatePackage.cmd";
 
-        public static void Run(string workDirectory, string cmd, out int exitCode, out string standOutput)
+        public static void Run(string workDirectory, string cmd, out int exitCode, out string standOutput, bool output = true, bool exitWithOpenForm = false)
         {
             exitCode = int.MinValue;
             standOutput = null;
@@ -20,44 +30,144 @@ namespace Utilities
             using (Process p = new Process())
             {
                 p.StartInfo.FileName = CMDPATH;
-                p.StartInfo.UseShellExecute = false;        //是否使用操作系统shell启动
-                p.StartInfo.RedirectStandardInput = true;   //接受来自调用程序的输入信息
-                p.StartInfo.RedirectStandardOutput = true;  //由调用程序获取输出信息
-                p.StartInfo.RedirectStandardError = true;   //重定向标准错误输出
-                p.StartInfo.CreateNoWindow = false;          //不显示程序窗口
+                p.StartInfo.UseShellExecute = false;         //是否使用操作系统shell启动
+                //p.StartInfo.Arguments=$" / k {cmd}";
+
+                if (exitWithOpenForm)
+                {
+                    p.StartInfo.Arguments = $"/c start {cmd}";
+                }
+                else
+                {
+                    if (output)
+                    {
+                        p.StartInfo.Arguments = $"/c {cmd}";
+                    }
+                    else
+                    {
+                        p.StartInfo.Arguments = $"/k {cmd}";
+                    }
+                }
+
+                if (output)
+                {
+                    p.StartInfo.RedirectStandardInput = true;   //接受来自调用程序的输入信息
+                    p.StartInfo.RedirectStandardOutput = true;  //由调用程序获取输出信息
+                    p.StartInfo.RedirectStandardError = true;   //重定向标准错误输出
+                    p.StartInfo.CreateNoWindow = true;          //不显示程序窗口                   
+                }
+                else
+                {
+                    p.StartInfo.CreateNoWindow = false;          //不显示程序窗口
+                }
+
                 p.StartInfo.Verb = "RunAs";
+
+                if (workDirectory != null)
+                {
+                    if (Directory.Exists(workDirectory))
+                    {
+                        p.StartInfo.WorkingDirectory = workDirectory;
+                    }
+                    else
+                    {
+                        throw new DirectoryNotFoundException($"{workDirectory} not found");
+                    }
+                }
 
                 p.Start();//启动程序
 
-
-                if (Directory.Exists(workDirectory))
+                if (output)
                 {
-                    p.StandardInput.WriteLine($"cd {workDirectory}");
+                    standOutput = p.StandardOutput.ReadToEnd();
+                    string error = p.StandardError.ReadToEnd();
+
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        error = Environment.NewLine + error;
+                    }
+
+                    standOutput += error;
+
+                    p.WaitForExit();//等待程序执行完退出进程
+
+                    exitCode = p.ExitCode;
                 }
-                else if (workDirectory != null)
-                {
-                    throw new DirectoryNotFoundException($"{workDirectory} not found");
-                }
-
-                //向cmd窗口写入命令
-                p.StandardInput.WriteLine(cmd);
-
-                p.StandardInput.WriteLine("exit");
-
-                p.StandardInput.AutoFlush = true;
-
-                //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
-                //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
-
-                //获取cmd窗口的输出信息
-                standOutput = p.StandardOutput.ReadToEnd();
-
-                p.WaitForExit();//等待程序执行完退出进程
-
-                exitCode = p.ExitCode;
 
                 p.Close();
             }
+        }
+
+        public static CommandResult Run(string workDirectory, string cmd, bool output = true)
+        {
+            Run(workDirectory, cmd, out int exitCode, out string standOutput, output);
+            CommandResult commandResult = new CommandResult(exitCode, standOutput);
+            return commandResult;
+        }
+
+        public static void RunWitheWDK(string workDirectory, string cmd, out int exitCode, out string standOutput, bool output = true, bool exitWithOpenForm = false)
+        {
+
+            RuneWDK();
+
+            Run(workDirectory, cmd, out exitCode, out standOutput, output, exitWithOpenForm);
+        }
+
+        public static CommandResult RunWitheWDK(string workDirectory, string cmd, bool output = true)
+        {
+            RunWitheWDK(workDirectory, cmd, out int exitCode, out string standOutput, output);
+            CommandResult commandResult = new CommandResult(exitCode, standOutput);
+            return commandResult;
+        }
+
+        public static void RuneWDK()
+        {
+            Process eWDKProcess = Process.GetProcessesByName(CMD).FirstOrDefault(p => p.MainWindowTitle == MAINWINDOWSTIELE);
+
+            if (eWDKProcess == null)
+            {
+                Run(EWDKPATH, EWDKCMD, out int exitCode, out string standOutput, false);
+            }
+        }
+
+        public static void OpenReposSln(string projectName)
+        {
+            if (!File.Exists(Path.Combine(REPOSFOLDER, projectName, REPOSSLN)))
+            {
+                throw new FileNotFoundException($"{REPOSSLN} not found", Path.Combine(REPOSFOLDER, projectName, REPOSSLN));
+            }
+
+            RunWitheWDK(Path.Combine(REPOSFOLDER, projectName), OPENREPOSSLN, out int exitCode, out string standOutput, false, true);
+        }
+
+        public static CommandResult CreatePacakge(string projectName, bool output = true)
+        {
+            return RunWitheWDK(Path.Combine(REPOSFOLDER, projectName), CREATEPACAKGE, output);
+        }
+
+        public struct CommandResult
+        {
+            public CommandResult(int exitCode, string standOutput)
+            {
+                m_ExitCode = exitCode;
+                m_StandOutput = standOutput;
+                m_ErrorOutput = null;
+            }
+
+            public CommandResult(int exitCode, string standOutput, string errorOutput)
+            {
+                m_ExitCode = exitCode;
+                m_StandOutput = standOutput;
+                m_ErrorOutput = errorOutput;
+            }
+
+            private int m_ExitCode;
+            private string m_StandOutput;
+            private string m_ErrorOutput;
+            public int ExitCode => m_ExitCode;
+            public string StandOutput => m_StandOutput;
+
+            public string ErrorOutput => m_ErrorOutput;
         }
     }
 }
